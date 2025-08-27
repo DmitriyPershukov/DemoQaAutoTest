ced0c0a7e5934f9b578653982d2d642291ae1042
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-08-27 17:41:54 +0500

    implemented WebDriverProvider; implemented WebDriverFactory

---
+++ task3/framework/web_driver/WebDriverFactory.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace task3.framework.web_driver
+{
+    internal class WebDriverFactory
+    {
+        public static IWebDriver GetWebDriver(string browserName)
+        {
+            switch (browserName)
+            {
+                case "Chrome":
+                    return new ChromeDriver();
+
+                case "Firefox":
+                    return new FirefoxDriver();
+
+                default:
+                    throw new ArgumentException(
+                        String.Format("Provided name '{0}' does not correspond to any available browser",
+                        browserName));
+            }
+        }
+    }
+}
---
+++ task3/framework/web_driver/WebDriverProvider.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using task3.framework.config.config_data_models;
+using task3.framework.config.config_utils;
+
+namespace task3.framework.web_driver
+{
+    internal class WebDriverProvider
+    {
+        private static IWebDriver? instance;
+        private WebDriverProvider() { }
+
+        public static IWebDriver GetInstance()
+        {
+            if (instance == null)
+            {
+                Config? config = ConfigManager.GetConfigurationModel<Config>();
+                instance = WebDriverFactory.GetWebDriver(config.Browser);
+            }
+            return instance;
+        }
+
+        public static void SetInstanceNull()
+        {
+            if (instance != null)
+            {
+                instance.Quit();
+            }
+            instance = null;
+        }
+    }
+}
