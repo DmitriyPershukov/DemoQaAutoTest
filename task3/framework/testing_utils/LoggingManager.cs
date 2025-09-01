b05c16e9ccf1ef5db790e329eb809e6e4406fc41
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-09-01 14:34:46 +0500

    implemented LoggingManager

---
+++ task3/framework/testing_utils/LoggingManager.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3.framework.testing_utils
+{
+    public class LoggingManager
+    {
+        private static Logger logger = NLog.LogManager.GetLogger("my logger name");
+
+        public static Logger GetLogger()
+        {
+            return logger;
+        }
+
+        public static void Shutdown()
+        {
+            LogManager.Shutdown();
+        }
+    }
+}
