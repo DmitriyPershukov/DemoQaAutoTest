6b6dd7c2cd3ed8fb087c773104cbed096bf0231b
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-09-02 17:27:51 +0500

    added RegistrationFormAddDelete class; implemented test data model for RegistrationFormAddDelete

---
+++ task3/test/tests/registration_form_add_delete/RegistrationFormAddDelete.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task3.framework.config_utils;
+using task3.framework.element_utils;
+using task3.framework.page;
+using task3.framework.test;
+using task3.framework.web_driver;
+using task3.test.pages;
+using task3.test.tests.global_test_data;
+using task3.test.tests.iframe_has_correct_text;
+
+namespace task3.test.tests.registration_form_add_delete
+{
+    public class RegistrationFormAddDelete : BaseTest
+    {
+        MainPage mainPage;
+        PlaygroundPage playgroundPage;
+        RegistrationFormAddDeleteTestData testData;
+
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+            mainPage = new MainPage("Main Page");
+            playgroundPage = new PlaygroundPage("Playground Page");
+            testData = TestDataManager.GetTestDataModel<RegistrationFormAddDeleteTestData>();
+        }
+
+        [TearDown]
+        public override void Teardown()
+        {
+            base.Teardown();
+        }
+
+        [Test]
+        public void RegistrationFormAddDeleteTest()
+        {
+            WebDriverProvider.GetInstance().Navigate().GoToUrl(globalTestData.MainPageURL);
+            Assert.That(mainPage.IsOpened(), "After navigating to main page it was not opened.");
+
+            mainPage.ClickAlertsWindowsButton();
+            playgroundPage.ClickAccordionMenuElement("Elements", "Web Tables");
+            Assert.That(playgroundPage.IsOpened() && true,
+                "Alerts, Frame & Windows button was clicked and button 'Nested Frames' was clicked in left menu " +
+                "but Nested Frames form was not opened.");
+        }
+    }
+}
---
+++ task3/test/tests/registration_form_add_delete/RegistrationFormAddDeleteTestData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task3.framework.config_utils;
+
+namespace task3.test.tests.registration_form_add_delete
+{
+    public class RegistrationFormAddDeleteTestData : IConfig
+    {
+        private const string ConfigFilePath =
+            "test\\tests\\registration_form_add_delete\\registration_form_add_delete_test_data.json";
+        public static string GetConfigFilePath()
+        {
+            return ConfigFilePath;
+        }
+
+        public int UserNumber;
+        public string? FirstName;
+        public string? LastName;
+        public string? Email;
+        public int Age;
+        public int Salary;
+        public string? Department;
+    }
+}
