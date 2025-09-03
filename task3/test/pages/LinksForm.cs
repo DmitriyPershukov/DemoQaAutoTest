89666abe17affeaa808df17f5dc89cbc94e999cb
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-09-03 18:12:34 +0500

    added class LinksForm

---
+++ task3/test/pages/LinksForm.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task3.framework.element_utils;
+using task3.framework.page;
+
+namespace task3.test.pages
+{
+    internal class LinksForm : BaseForm
+    {
+        BaseButton homeLink;
+        public LinksForm(string name) : base(
+            new BareElement(By.Id("linkWrapper"), "LinksForm identifying element"), name)
+        {
+            homeLink = new BaseButton(By.Id("simpleLink"), "Home Link");
+        }
+
+        public void ClickHomeLink()
+        {
+            homeLink.Click();
+        }
+    }
+}
