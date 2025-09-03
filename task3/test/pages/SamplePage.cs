221134d78012f7a0377154663c1983d93a2d525b
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-09-03 16:37:51 +0500

    implemented sample page class

---
+++ task3/test/pages/SamplePage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task3.framework.element_utils;
+using task3.framework.page;
+
+namespace task3.test.pages
+{
+    public class SamplePage : BaseForm
+    {
+        public SamplePage(string name) : base(
+            new BareElement(By.Id("//*[@id='sampleHeading']"), "SamplePage identifying element"), name)
+        {}
+    }
+}
