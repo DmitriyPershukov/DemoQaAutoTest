e057fc558904111737c3f833e56a045c003cfdbf
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-09-03 14:26:26 +0500

    implemented element InputField

---
+++ task3/test/elements/InputField.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task3.framework.element_utils;
+using task3.framework.testing_utils;
+
+namespace task3.test.elements
+{
+    internal class InputField : BaseElement
+    {
+        public InputField(By locator, string name) : base(locator, name)
+        {
+        }
+
+        public void SendKeys(string text)
+        {
+            LoggingManager.GetLogger().Debug($"Entering text '{text}' into input field {Name}");
+            GetElement().SendKeys(text);
+        }
+    }
+}
