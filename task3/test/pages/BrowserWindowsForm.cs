1c5ba27313dfdb05e559ca836153386090160744
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-09-03 16:23:04 +0500

    added class BrowserWindowsForm

---
+++ task3/test/pages/BrowserWindowsForm.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task3.framework.element_utils;
+using task3.framework.page;
+
+namespace task3.test.pages
+{
+    public class BrowserWindowsForm : BaseForm
+    {
+        public BrowserWindowsForm(string name) : base(
+            new BareElement(By.Id("browserWindows"), "BrowserWindowsForm identifying element"), name)
+        {}
+    }
+}
