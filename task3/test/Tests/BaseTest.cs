883a23c8266d5b07591a3d35892605120634db49
DmitriyPershukov <dmitriypershukov@gmail.com>
2025-09-05 18:42:59 +0500

    moved BaseTest to Test

--- task3/test/Tests/BaseTest.cs
+++ task3/test/Tests/BaseTest.cs
@@ -1,13 +1,17 @@
-﻿using task3.framework.web_driver;
+﻿using NUnit.Framework;
+using task3.framework.web_driver;
 using task3.Framework.Logging;
 using task3.Framework.Utils.ConfigUtils;
 using task3.test.tests.global_test_data;
 
 namespace task3.framework.test
 {
+    [TestFixture]
     abstract public class BaseTest
     {
         protected GlobalTestData globalTestData;
+
+        [SetUp]
         public virtual void Setup()
         {
             LogTestStart();
@@ -15,6 +19,7 @@ namespace task3.framework.test
             
         }
 
+        [TearDown]
         public virtual void Teardown()
         {
             WebDriverProvider.SetInstanceNull();
